72d5889312db6d1c1f95bc9d4e3ad40730c9c18b
mewtwo <87859323+ashu540@users.noreply.github.com>
2021-12-15 09:32:24 +0530

    Create data_type.java

---
+++ data_type.java
@@ -0,0 +1,39 @@
+class HelloWorld {
+    public static void main(String[] args) {
+       
+        
+        byte b=10;
+        short s=120;
+        int i=12433;
+        long Lo=4224242423L;
+        float f=3.456f;
+        double d=5674.678;
+        char c='A';
+        boolean bo=true;
+
+        System.out.println("the values are-->");
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(Lo);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(bo);
+
+
+
+
+    }
+}
+
+output-->
+10
+120
+12433
+4224242423
+3.456
+5674.678
+A
+true
